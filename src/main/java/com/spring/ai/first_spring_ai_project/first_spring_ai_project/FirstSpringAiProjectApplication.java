e494b64e0ac98df5acca5c12c4f56cd7157e5caf
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-05-03 16:51:16 +0530

    initial commit

---
+++ src/main/java/com/spring/ai/first_spring_ai_project/first_spring_ai_project/FirstSpringAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.first_spring_ai_project.first_spring_ai_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/ai/first_spring_ai_project/first_spring_ai_project/FirstSpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.first_spring_ai_project.first_spring_ai_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
